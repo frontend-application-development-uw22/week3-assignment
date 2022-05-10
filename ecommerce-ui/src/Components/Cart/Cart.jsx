71f94b99263e64701f340b970024546c4b41ff5a
Admin <admin@Admins-MacBook-Pro.local>
2022-05-10 11:41:40 -0700

    Added cost details to Cart components.

--- ecommerce-ui/src/Components/Cart/Cart.jsx
+++ ecommerce-ui/src/Components/Cart/Cart.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import CartCard from "./CartCard";
+import "./Cart.css";
 
 export default function Cart({ cartItems, onRemoveFromCartHandler }) {
-  console.log(cartItems);
+  let totalCost = cartItems.reduce((total, individual) => {
+    return (total += individual.payment.cost);
+  }, 0);
+
   return (
     <>
-      <h1>Cart Items</h1>
+      <div className="cart-header">
+        <h1 className="cart-header__title">Cart Items</h1>
+        <p className="cart-header__total-cost">Total Cost = ${totalCost}</p>
+      </div>
       {cartItems.length === 0 && <h3>Your cart is currently empty</h3>}
       {cartItems.map((property) => {
         return (
--- ecommerce-ui/src/Components/Cart/CartCard.jsx
+++ ecommerce-ui/src/Components/Cart/CartCard.jsx
@@ -4,22 +4,27 @@ import "./CartCard.css";
 export default function CartCard({ onRemoveFromCartHandler, property }) {
   return (
     <div className="cart-card">
-      <img
-        className="cart-card__image"
-        src={property.image}
-        alt={property.title}
-      />
       <div className="cart-card__details">
-        <h3 className="cart-card__title">{property.title}</h3>
-        <p className="cart-card__location">
-          {property.location.city}, {property.location.country}
-        </p>
-        <button
-          className="cart-card__button"
-          onClick={() => onRemoveFromCartHandler(property)}
-        >
-          Remove from cart
-        </button>
+        <img
+          className="cart-card__image"
+          src={property.image}
+          alt={property.title}
+        />
+        <div className="cart-card__info">
+          <h3 className="cart-card__title">{property.title}</h3>
+          <p className="cart-card__location">
+            {property.location.city}, {property.location.country}
+          </p>
+          <button
+            className="cart-card__button"
+            onClick={() => onRemoveFromCartHandler(property)}
+          >
+            Remove from cart
+          </button>
+        </div>
+      </div>
+      <div className="cart-card__cost">
+        <p>Cost = ${property.payment.cost}</p>
       </div>
     </div>
   );
