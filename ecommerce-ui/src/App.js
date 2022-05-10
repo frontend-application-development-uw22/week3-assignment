f72c50e8159ac4434464e294ac350607d97b5755
Joel Elizaga <jelizaga@hal-9000.hsd1.wa.comcast.net>
2022-05-10 16:59:06 -0700

    Added basic <nav>.

--- ecommerce-ui/src/App.js
+++ ecommerce-ui/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import BnBCardList from './components/BnBCardList';
+import Nav from './components/Nav'
 import bnbData from './data/bnbs.json';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <BnBCardList bnbData={bnbData} />
     </div>
   );
