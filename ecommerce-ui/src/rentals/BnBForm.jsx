327ea0bdf5e0531f75f9e2d20096cf5b3b4c439f
pablomarcel <pablomarcel@outlook.com>
2022-05-12 14:39:58 -0700

    Final Commit

--- ecommerce-ui/src/rentals/BnBForm.jsx
+++ ecommerce-ui/src/rentals/BnBForm.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from "prop-types";
 
-
 export default function BnBForm({
                                   setTitle,
                                   sethouseType,
